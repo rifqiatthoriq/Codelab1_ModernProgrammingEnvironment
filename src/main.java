37e0926123c286a2206c72f3236b160ed1b2a855
rifqiatthoriq <rifqiatthoriq847@gmail.com>
2025-10-24 15:18:47 +0800

    Initial commit - Lab 1 project

---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Modul 3 - Lab 1!");
+        System.out.println("Program Java ini berhasil dijalankan tanpa error 🚀");
+    }
+}
